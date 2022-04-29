ce441b071b7da6698b869c1d87dc1e92fbc45e17
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2022-04-29 15:32:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker} from "react-google-maps";
 import Home from './pages';
 import Information from './pages/Information';
 import Reservations from './pages/Reservations';
@@ -104,7 +104,7 @@ function App() {
       
       <div style = {{width: '100vw', height: '100vh'}}>
         <WrappedMap
-          googleMapURL = {'https://maps.googleapis.com/maps/api/js?key=AIzaSyDYxDVoaLN3_ZF4TDICgharUUQtly0lnK8&v=3.exp&libraries=geometry,drawing,places'}
+          googleMapURL = {'https://maps.googleapis.com/maps/api/js?&v=3.exp&libraries=geometry,drawing,places'}
           loadingElement = {<div style = {{height: "100%"}}/>}
           containerElement = {<div style = {{ height: "100%"}} />}
           mapElement = {<div style = {{ height:"100%"}} />}
