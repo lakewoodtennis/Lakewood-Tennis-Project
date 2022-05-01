6b0cdddc045903b0d71412ab38bfd4377eeb8ef0
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2022-04-30 17:34:04 -0700

    buttons and maps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,19 +11,30 @@ import Staff from './pages/Staff';
 import Tournaments from './pages/Tournaments';
 import cursedtennis from './images/tenniscourt.webp';
 import silva from './images/silva.jpg';
+import tem from './images/tem.jpg';
+import Popup from './popupWindows/Popup';
+import './popupWindows/buttonStyle.css';
 
 function Map(){
   return(
     <GoogleMap
     defaultZoom = {10}
-    defaultCenter = {{lat:33.83433 , lng:-118.15502}}
-    />
+    defaultCenter = {{lat:33.83433 , lng:-118.15502}}>
+    <Marker key = "tennisLocation" position = {{lat:33.83433 , lng:-118.15502}}/>
+    </GoogleMap>
+    
   );
 }
 
 const WrappedMap  = withScriptjs(withGoogleMap(Map));
 
 function App() {
+  const[isOpen,setIsOpen] = useState(false);
+
+  const togglePopup = () =>{
+    setIsOpen(!isOpen);
+  }
+
   return (
     <Router>
       <Navbar />
@@ -44,10 +55,10 @@ function App() {
             tennis and tennis courts that are rented out for playing tennis and instructors that teach you how to play tennis.
             Legend has it the Lakewood Tennis Center is located in Lakewood, California. Feel free to explore this website to find
             more information such as hours, information about staff, ongoing events, and make a reservation!
-            Don't let your dreams be dreams go out and play some tennis. 🎾
+            Don't let your dreams be dreams go out and play some tennis. <span role="img" aria-label = "racket">🎾</span>
         </p1>
         <h1 align='center'>‎</h1>
-        <img className="photo"  src={cursedtennis}></img>
+        <img className="photo" alt =""  src={cursedtennis}></img>
       </div>
 
       {/*Shop info portion*/}
@@ -76,8 +87,9 @@ function App() {
         <h className = "profiles" style = {{fontWeight:'bold', fontSize: 50}}>Instructor Profiles</h>
       </div>
 
-      <div>
-        <h style = {{fontWeight: 'bold', fonsize: 30}}>VICTOR SILVA, DIRECTOR AND HEAD COACH OF SILVA ACADEMY</h>
+      <div style = {{width:'25vw'}}>
+        <h style = {{fontWeight: 'bold', fontSize: 30}}>Victor Silva</h>
+        <img src = {silva} alt = "Victor Silva"></img>
         <p>Started playing in Sao Paolo, Brazil, coached by his father, Edson. 
           He has played tennis since he was five years old and started to compete 
           seriously at 12. He won 10 national tournaments in Brazil and more 
@@ -85,11 +97,48 @@ function App() {
           and earned his first ATP point shortly thereafter. Victor came to California 
           at 17 years of age, played at Wilson High School in Long Beach,then two years 
           of college at Long Beach State. Victor started Silva Tennis Academy in 2020.</p>
-        <img src = {silva} alt = "Victor Silva"></img>
+        <input 
+          type = "button" 
+          value = "Click for Contact Info" 
+          onClick = {togglePopup}
+        />
+        {isOpen && <Popup
+          content = {<>
+            <p>Contact info here</p>
+          </>}
+        handleClose = {togglePopup}
+        />}
+        
       </div>
 
-      <div>
-        <h style = {{fontWeight: 'bold', fontsize:30}}>Contact Info</h>
+      <div style = {{width: '25vw', }}>
+        <h style = {{fontWeight: 'bold', fontSize: 30}}>Tem Sykahua</h>
+        <br></br>
+        <img src = {tem} height = {500} width = {'125%'} alt = "Tem Sykahua"></img>
+        <p>Tem Sykahua is a former Men’s Open Champion. He has won many tournaments that include: 
+           Men’s Open, NTRP 5.5, Junior College, College Division 3, Junior Open and Satellite, 
+           Masters, etc. With over 30 years of playing experience, Tem has worked with top level 
+           coaches and athletes to share his knowledge of the game. He is a current member of the 
+           United States Professional Tennis Association. Tem is also an inductee to Lakewood’s 
+           Hall of Fame for his new found success on students winning tournaments.</p>
+           <input 
+          type = "button" 
+          value = "Click for Contact Info" 
+          onClick = {togglePopup}
+        />
+        {isOpen && <Popup
+          content = {<>
+            <p>Location: Lakewood Tennis Center</p>
+            <p>Phone: (562) 355 - 2545</p>
+            <a href = "https://www.temsamazingtennis.com/" target = "_blank" rel="noreferrer noopener">My Website!</a>
+          </>}
+        handleClose = {togglePopup}
+        />}
+      </div>
+
+      <br></br>
+      <div >
+        <h style = {{fontWeight: 'bold', fontSize: 50}}>Contact Info</h>
       </div>
 
       <div class = "contactInfo">
@@ -101,15 +150,19 @@ function App() {
         {/*to get href tag*/}
         <a>Lakewood10s@gmail.com</a>
       </div>
+      <br></br>
       
-      <div style = {{width: '100vw', height: '100vh'}}>
+      <div style = {{width: '25vw', height: '25vh'}}>
         <WrappedMap
-          googleMapURL = {'https://maps.googleapis.com/maps/api/js?&v=3.exp&libraries=geometry,drawing,places'}
+          googleMapURL = {'https://maps.googleapis.com/maps/api/js?&v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDYxDVoaLN3_ZF4TDICgharUUQtly0lnK8'}
           loadingElement = {<div style = {{height: "100%"}}/>}
           containerElement = {<div style = {{ height: "100%"}} />}
           mapElement = {<div style = {{ height:"100%"}} />}
+          
         />
       </div>
+
+      <a style = {{fontSize: 50}}href = "https://www.google.com/maps/place/Lakewood+Tennis+Center/@33.8348484,-118.1568505,17z/data=!3m1!4b1!4m5!3m4!1s0x80dd32523cef5fb3:0x105827ec28f2a3cc!8m2!3d33.8348442!4d-118.1546727" target = "_blank" rel="noreferrer noopener">Get Directions</a>
   
 
     </Router>
