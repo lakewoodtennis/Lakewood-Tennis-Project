7b37f854cf9e35d7aa110086f9f2a95cd5c9a8a2
astrosteel8 <starkidchheang@yahoo.com>
2022-05-04 15:32:16 -0700

    Rules for the Singles ladder (still needs to be formatted)

--- src/App.js
+++ src/App.js
@@ -303,8 +303,17 @@ function App() {
       {/*SINGLES LADDER*/}
       <div style={infoStyle}>
         <h1 className = "singles" style = {{fontWeight:'bold', fontSize: 40}}> Singles Ladder </h1>
-        <p1>
-            Information about Singles Ladder
+        <p1 style = {{justifyContent: "flex", align : "left"}}>
+        Lakewood Tennis Ladder rules:<br/>                                              	
+        1.  Challenge Guidelines: Players may challenge upwards a maximum of three positions. Challengers must issue the challenge to higher players and matches must be accepted and scheduled within three weeks of the challenge, unless both players agree to an extension, or the ladder director (Head Pro or Tennis Director) accepts a reasonable reason for a delay such as a challenged player being out of town or injured. In any event, all matches must be played within five weeks, or the challenged player is dropped from the ladder.<br/> 
+        2.  Time Between Challenges: To avoid excess challenges against any single player, no player may be challenged within one week of having played any challenge match.<br/> 
+        3.   Challenger Responsibilities: Challengers will make all needed court reservations. Please reserve a minimum of two hours when making reservations. (Players will split the court fees) The challenger will supply a new can of balls for the match. All matches are to be played at Lakewood Tennis Center. The time of matches must be convenient for both players.<br/> 
+        4.   Reporting Scores: Winners of matches must report scores to the ladder director within 48 hours of the match.  Email to Eric lakewood10s@gmail.com<br/> 
+        5.   Post-Match Positions: If the higher position player wins the match, there is no change of position. If the challenger wins the match, he or she moves into the loser's position, and everyone in between moves down one place.<br/> 
+        6.   Playing Format: All matches will be the best of two sets.  If you split sets, it will be determined by a “Match Tie-Break” up to 10.  All other aspects of fair play and conduct will follow USTA guidelines. Matches stopped for bad weather or similar reasons must be completed within one week or the leading player wins.<br/> 
+        7.   Joining the Ladder: A person joining an existing ladder has one initial chance to challenge anyone in any position on the ladder. If he or she loses that challenge match they will then be placed on the bottom of the ladder and must challenge to move up. The new ladder participant who wins that initial match moves into the position of the person they just defeated.<br/> 
+        8.  This ladder is free other than the court fees.
+        
         </p1>
 
         {/*functionality of showing singles ladder info in a table format*/}
