1e73c5b1191a3e5b818af9e3944fcef59d9e928d
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2022-05-04 19:18:20 -0700

    changed padding on navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,29 +14,29 @@ const Navbar = () => {
 
   return (
     <>
-      <Nav style = {{justifyContent: 'space-evenly'}}>
+      <Nav style = {{justifyContent: 'space-evenly', width: '100%'}}>
       <Link activeClass = "active" to = "home" spy = {true} smooth = {true} offset = {-100} delay = {0} >
           <img className="navLogo" src={require('../../images/logotrans.png')} alt='logo'/>
         </Link>
         <Bars />
         <NavMenu>
-          <Link activeClass = "active" to = "shopInfo" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "shopInfo" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 10, color:"white", fontSize: 20}}>
             Information
           </Link>
-          <Link activeClass = "active" to = "reservations" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "reservations" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 35, color:"white", fontSize: 20}}>
             Reservations
           </Link>
-          <Link activeClass = "active" to = "Tournaments" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "Tournaments" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 35, color:"white", fontSize: 20}}>
             Tournaments
           </Link>
-          <Link activeClass = "active" to = "singles" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "singles" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 35, color:"white", fontSize: 20}}>
             Singles Ladder
           </Link>
-          <Link activeClass = "active" to = "profiles" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "profiles" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 35, color:"white", fontSize: 20}}>
             Instructors
           </Link>
         
-          <Link activeClass = "active" to = "contactInfo" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 40, color:"white", fontSize: 20}}>
+          <Link activeClass = "active" to = "contactInfo" spy = {true} smooth = {true} offset = {-100} delay = {0} style = {{padding: 20, color:"white", fontSize: 20}}>
             Contact Us
             </Link>
           </NavMenu>
